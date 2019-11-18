0938f4ecba2d31534ae794ada616fef2a9601aa9
yuhBert <star808fer@gmail.com>
2019-11-18 10:00:01 +0800

    layu pami

--- src/components/frame/Header.vue
+++ src/components/frame/Header.vue
@@ -2,13 +2,23 @@
   <div fixed="top">
     <b-card no-body>
       <b-card-header header-tag="nav" class="bg-success">
-        <b-nav class="card-header tabs" >
-         
-          <b-nav-item  v-if="auth.user != null">Easakay</b-nav-item>
+        <b-nav class="card-header tabs">
+          <b-nav id="Logotext" v-if="auth.user != null">Easakay</b-nav>
 
-          <b-nav @click="redirect('/Login')" v-if="auth.user ===null" id="text" type="button" class="btn btn-primary">&nbsp;&nbsp;Sign In</b-nav>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <b-nav @click="redirect('/Register')" v-if="auth.user ===null" id="text" type="button" class="btn btn-primary">&nbsp;&nbsp;Sign Up</b-nav>
+          <b-nav
+            @click="redirect('/Login')"
+            v-if="auth.user ===null"
+            id="text"
+            type="button"
+            class="btn btn-primary"
+          >&nbsp;&nbsp;Sign In</b-nav>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <b-nav
+            @click="redirect('/Register')"
+            v-if="auth.user ===null"
+            id="text"
+            type="button"
+            class="btn btn-primary"
+          >&nbsp;&nbsp;Sign Up</b-nav>
         </b-nav>
       </b-card-header>
     </b-card>
@@ -22,9 +32,14 @@
 }
 .card-header tabs {
   padding-bottom: 50%;
-   padding-top: 50%;
+  padding-top: 50%;
 }
-
+#Logotext {
+  color: white;
+  font-size: 20px;
+  font-style: Impact, Charcoal, sans-serif;
+  font-weight: bold;
+}
 </style>
 <script>
 import AUTH from "services/auth";
--- src/modules/Dashboard.vue
+++ src/modules/Dashboard.vue
@@ -1,55 +1,84 @@
 <template>
- <b-jumbotron bg-variant="info" text-variant="white" border-variant="dark">
-
- 
- <b-container class="bv-example-row">
-  <b-row>
-    <b-col> <div class="col-md-12">
-    <label for="input-live">From</label>
-    <b-form-input
-      id="input-live"
-      v-model="name"
-      aria-describedby="input-live-help input-live-feedback"
-    ></b-form-input>
-</div></b-col>
-    <b-col> <div class="col-md-12">
-    <label for="input-live">To</label>
-    <b-form-input
-      id="input-live"
-      v-model="name"
-      aria-describedby="input-live-help input-live-feedback"
-    ></b-form-input>
-</div></b-col>
-  <b-col>
-<div class="col-md-12">
-    <label for="input-live">Depart Time</label>
-    <b-form-input
-      id="input-live"
-      v-model="name"
-      aria-describedby="input-live-help input-live-feedback"
-    ></b-form-input>
-</div>
-</b-col>
-
-
-  </b-row><br>
-
-
-</b-container>
-</b-jumbotron>
-
+  <b-jumbotron id="header" bg-variant="white" text-variant="black">
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col>
+          <div class="col-md-12">
+            <label for="input-live">From</label>
+            <b-form-input
+              id="input-live"
+              v-model="from"
+              aria-describedby="input-live-help input-live-feedback"
+            ></b-form-input>
+          </div>
+        </b-col>
+        <b-col>
+          <div class="col-md-12">
+            <label for="input-live">To</label>
+            <b-form-input
+              id="input-live"
+              v-model="to"
+              aria-describedby="input-live-help input-live-feedback"
+            ></b-form-input>
+          </div>
+        </b-col>
+        <b-col>
+          <div class="col-md-12">
+            <label for="input-live">Depart Time</label>
+            <b-form-input
+              id="input-live"
+              v-model="depart"
+              aria-describedby="input-live-help input-live-feedback"
+            ></b-form-input>
+          </div>
+        </b-col>
+        <b-col>
+          <div class="col-md-12">
+            <b-button id="b-button" variant="outline-success">SEARCH</b-button>
+          </div>
+        </b-col>
+      </b-row>
+      <br>
+    </b-container>
+    <div>
+      <b-dropdown id="dropdown-1" text="Dropdown Button" class="m-md-2">
+        <b-dropdown-item>First Action</b-dropdown-item>
+        <b-dropdown-item>Second Action</b-dropdown-item>
+        <b-dropdown-item>Third Action</b-dropdown-item>
+        <b-dropdown-divider></b-dropdown-divider>
+        <b-dropdown-item active>Active action</b-dropdown-item>
+        <b-dropdown-item disabled>Disabled action</b-dropdown-item>
+      </b-dropdown>
+    </div>
+    
+  </b-jumbotron>
 </template>
+
+
+<style scoped>
+#header {
+  margin-top: 1%;
+  padding-bottom: 2%;
+  padding-top: 2%;
+  border: solid green 2px;
+}
+#b-button {
+  margin-top: 13%;
+}
+</style>
+
+
 <script>
-  export default {
-    computed: {
-      nameState() {
-        return this.name.length > 2 ? true : false
-      }
-    },
-    data() {
-      return {
-        name: ''
-      }
+export default {
+  computed: {
+    nameState() {
+      return this.name.length > 2 ? true : false;
     }
+  },
+  data() {
+    return {
+      name: ""
+    };
   }
+};
 </script>
